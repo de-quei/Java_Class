fc63083bdb1b28d11ad64fc3a0c2d2f889fb7189
Hyunji Kim <108276366+de-quei@users.noreply.github.com>
2022-10-24 14:36:18 +0900

    Update TwoBox.java

--- JavaPrg/src/Class/TwoBox.java
+++ JavaPrg/src/Class/TwoBox.java
@@ -1,27 +1,37 @@
 package Class;
+import java.util.Scanner;
 class Box{
 	int width;
 	int height;
 	int depth;
-	void calc_vol(){
-		int volume = width * height * depth;
-		System.out.println("박스의 부피 : " + volume);
+	int calc_vol(){
+		return width * height * depth;
+	}
+	Box(){
+		
+	}
+	Box(int w, int h, int d){
+		width = w;
+		height = h;
+		depth = d;
 	}
 }
 public class TwoBox {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Box box1 = new Box();
-		box1.width = 5;
-		box1.height = 4;
-		box1.depth = 7;
+		Scanner sc = new Scanner(System.in);
+		Box box1 = new Box(3, 4, 5);//생성자
+		Box box3 = new Box(1, 2, 3);
+		//box1.width = sc.nextInt();
+		//box1.height = sc.nextInt();
+		//box1.depth = sc.nextInt();
 		
-		Box box2 = new Box();
-		box2.width = 3;
-		box2.height = 5;
-		box2.depth = 9;
+		Box box2 = new Box(1, 2, 3);
+		//box2.width = sc.nextInt();
+		//box2.height = sc.nextInt();
+		//box2.depth = sc.nextInt();
 		
-		box1.calc_vol();
-		box2.calc_vol();
+		System.out.println("박스1의 부피 : " + box1.calc_vol());
+		System.out.println("박스2의 부피 : " + box2.calc_vol());
 	}
 }
