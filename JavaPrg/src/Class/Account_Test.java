c82e6034baf693e6492d2d53af41e91cb4e59e38
Hyunji Kim <108276366+de-quei@users.noreply.github.com>
2022-11-21 14:21:02 +0900

    Update Account_Test.java

--- JavaPrg/src/Class/Account_Test.java
+++ JavaPrg/src/Class/Account_Test.java
@@ -1,9 +1,8 @@
-//클래스 좀 더 연습해오겠습니다....
 package Class;
 class Account1 {
 	private String acc_num;
 	private int balance;
-	
+	static final double interest = 0.02;
 	Account1(){
 		
 	}
@@ -22,12 +21,18 @@ class Account1 {
 		}
 	}
 	public void printMyAccount() {
-		System.out.println("==============");
+		System.out.println("========================");
 		System.out.println("계좌번호 : " + this.acc_num);
-		System.out.println("잔    액 : " + this.balance);
+		System.out.println("잔   액 : " + this.balance);
 	}
 	public String toString() {
 		return "계좌번호 : " + acc_num + ", 현재잔액 : " + balance+"원";
+	}
+	void fixedDeposit(int year, int money1) {
+		System.out.println("========================");
+		int principle = money1 * 12 * year; //원금
+		double res =  principle + (principle * (year * interest)) ;
+		System.out.println("이자 : " + res + "원");
 	}
 	
 }
@@ -38,11 +43,14 @@ public class Account_Test {
 		System.out.println(ac1.toString());
 		Account1 ac2 = new Account1("10-02-33-1457", 32000);
 		System.out.println(ac2.toString());
+		Account1 ac3 = new Account1();
 		
 		ac1.deposit(12000);
 		ac2.withdraw(10000);
 		ac1.printMyAccount();
 		ac2.printMyAccount();
+		
+		ac3.fixedDeposit(2, 200000);
 	}
 
 }
